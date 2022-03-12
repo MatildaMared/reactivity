feeee715ef20d40187b14c328eebc2ed5c6d6302
Matilda Mared <matildamared@live.se>
2022-03-12 16:51:29 +0100

    add cors policy to backend

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [activities, setActivities] = useState([]);
+
+	async function getActivities() {
+		const response = await axios.get("http://localhost:5000/api/activities");
+		console.log(response);
+		setActivities(response.data);
+	}
+
+	useEffect(() => {
+		getActivities();
+		console.log(activities);
+	}, []);
+
+	return (
+		<div className="App">
+			<ul>
+				{activities.map((activity: any) => (
+          <div key={activity.id}>
+            <h1>{activity.title}</h1>
+          </div>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
