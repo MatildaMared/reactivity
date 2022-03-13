6b29e39d77ce78e8bfef0a0042004146415798ab
Matilda Mared <matildamared@live.se>
2022-03-13 13:47:15 +0100

    add functionality to update activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -25,4 +25,11 @@ public class ActivitiesController : BaseApiController
     {
         return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
     }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+    {
+        activity.Id = id;
+        return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+    }
 }
