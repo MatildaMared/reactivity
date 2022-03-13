7f1d6e13623080e70f3496c798b48343d09e8509
Matilda Mared <matildamared@live.se>
2022-03-13 12:09:31 +0100

    implement ability to get single activity with mediator

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -17,6 +17,6 @@ public class ActivitiesController : BaseApiController
     [HttpGet("{id}")] //activities/:id
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
-        return Ok();
+        return await Mediator.Send(new Details.Query { Id = id });
     }
 }
