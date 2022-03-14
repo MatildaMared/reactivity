cd7834e058a3d3ad9d9b0a1578a08715328b3a39
Matilda Mared <matildamared@live.se>
2022-03-14 18:55:39 +0100

    add ActivityForm component

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -3,7 +3,7 @@ import { Button, ButtonGroup, Card, Icon, Image } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 
 interface Props {
-  activity: Activity;
+	activity: Activity;
 }
 
 function ActivityDetails({ activity }: Props) {
@@ -11,19 +11,17 @@ function ActivityDetails({ activity }: Props) {
 		<Card fluid>
 			<Image src={`./assets/categoryImages/${activity.category}.jpg`} />
 			<Card.Content>
-        <Card.Header>{activity.title}</Card.Header>
+				<Card.Header>{activity.title}</Card.Header>
 				<Card.Meta>
-          <span>{activity.date}</span>
+					<span>{activity.date}</span>
 				</Card.Meta>
-				<Card.Description>
-					{activity.description}
-				</Card.Description>
+				<Card.Description>{activity.description}</Card.Description>
 			</Card.Content>
 			<Card.Content extra>
-        <ButtonGroup widths="2">
-          <Button basic color="blue" content="Edit" />
-          <Button basic color="grey" content="Cancel" />
-        </ButtonGroup>
+				<ButtonGroup widths="2">
+					<Button basic color="blue" content="Edit" />
+					<Button basic color="grey" content="Cancel" />
+				</ButtonGroup>
 			</Card.Content>
 		</Card>
 	);
