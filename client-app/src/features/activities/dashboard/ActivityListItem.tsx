a88d0e7111368acf0188ab37760670e0c902ba19
Matilda Mared <matildamared@live.se>
2022-03-18 06:54:58 +0100

    add styling for home page and clean upp unnecessary imports

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
